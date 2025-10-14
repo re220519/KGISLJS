7b9e576c5cb819cf1cd458a37ea7cf18740fbc75
re220519 <kreshma.k54@gmail.com>
2025-10-14 12:26:43 +0400

    adding callback

--- callback2.js
+++ callback2.js
@@ -1,17 +1,30 @@
 function greet(name,callback){
-    console.log("Hello",name);
-    callback();
-}//Write a function greet(name, callback) that prints "Hello, name" using the callback.
+   // console.log("Hello"+name);
+    callback(name);
+}
+function displayName(value){
+    console.log("hello:",value); 
+}
+greet("reshma", displayName);
+//Write a function greet(name, callback) that prints "Hello, name" using the callback.
 
 function sum(a,b,callback){
     let result = a+b;
-    console.log("Result:", result);
+  //  console.log("Result:", result);
     callback(result);
-}//Create a function sum(a, b, callback) that adds two numbers and passes the result to the callback for printing.
+}
+sum(2,3,function(value){
+    console.log(value);
+})
+//Create a function sum(a, b, callback) that adds two numbers and passes the result to the callback for printing.
 function multiply(a,b,callback){
    let product=a*b;
    callback(product);
-}//Write a function multiply(a, b, callback) that multiplies two numbers and logs the result via callback.
+}
+multiply(3,4,(value)=>{
+    console.log(value);
+})
+//Write a function multiply(a, b, callback) that multiplies two numbers and logs the result via callback.
 function fullName(firstName,lastName,callback){
    let name= firstName+" "+lastName;
     callback(name);
@@ -26,7 +39,11 @@ function checkEven(number,callback){
     callback(even);
 
 }
-function evenOdd(result){
+//function checkOdd(number,callback){
+//    let odd= number%2!==0
+//    callback(odd);
+//}
+function even(result){
     if(result){
         console.log("even");
     }
@@ -35,7 +52,7 @@ function evenOdd(result){
     }
 
 }
-checkEven(5,evenOdd);
+checkEven(5,even);
 //Write a function checkEven(number, callback) that checks if a number is even and sends true/false to callback.
 function goodMorning(callback){
     let message= "Good morning";
@@ -48,7 +65,7 @@ goodMorning(result)
 
 //Create a function greetMorning(callback) that passes "Good Morning" to the callback for printing.
 function calculateArea(length,width,callback){
-    let area= l*w;
+    let area= length*width;
     callback(area);
 }
 function rectangleArea(result){
@@ -61,8 +78,9 @@ function filterArray(arr,callback){
     let filter= arr.filter(function(num){
          return num>10;
     }) 
+    callback(filter);
 }
-callback(filter);
+
 function displayResult(result){
     console.log("filtered array:",result);
 }
@@ -94,3 +112,36 @@ performOperation(2,3,add);
 performOperation(3,4,substraction);
 performOperation(3,5,product);
  
+//Print "Hello after 2 seconds" using setTimeout().
+setTimeout(function(){
+    console.log("Hello");
+},2000);
+//Display "Task completed!" after a delay of 5 seconds.
+setTimeout(function(){
+    console.log("task Compleeted");
+},5000);
+//Show a countdown message after 3 seconds (e.g., "Starting now...").
+setTimeout(()=>{
+    console.log("Starting now...");
+},3000);
+//Use setTimeout() to print each word of "Learning JavaScript" with a 1-second delay between them.
+setTimeout(function(){
+    let word1="Learning Javascript"
+    let word2=word1.split(" ");
+    console.log(word2);
+},1000);
+
+//setInterval() – Tasks
+//Print "Running..." every 2 seconds using setInterval()
+/*setInterval(function(){
+    console.log("Running")
+},2000);*/
+//Create a counter that prints numbers from 1 to 10 at 1-second intervals.
+let count=0;
+let intervelId=setInterval(() => {
+    count++;
+    console.log(count);
+    if (count===10)
+    clearInterval(intervelId);
+    console.log(" ");
+}, 2000);
